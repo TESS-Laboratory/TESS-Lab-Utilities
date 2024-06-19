cd3bc92cdd03cd0fd20d9de0d35a4de5e8cc8fb2
GLENN SLADE <glennslade6@gmail.com>
2024-06-19 13:02:23 +0100

    path updates

--- Utility 2 - PDF Image Extractor/PDF_Image_Extractor.R
+++ Utility 2 - PDF Image Extractor/PDF_Image_Extractor.R
@@ -5,10 +5,15 @@ library("magick")
 
 # creating magick-image class with a png for each page of the pdf
 
-# NB change the working directory and inpiut/output directories and pdf location to your own in the following script
-setwd("C:/Workspace/R_Scripts/PDF_Image_Conversion")
+# NB change the working directory and input/output path directories and pdf name to your own in the following script
 
-pages <- magick::image_read_pdf("C:/Workspace/R_Scripts/PDF_Image_Conversion/data_in/dsa-2023-0003.pdf")
+input_path = "C:/Workspace/R_Scripts/PDF_Image_Conversion/data_in"# change to your input data directory
+
+output_path = "C:/Workspace/R_Scripts/PDF_Image_Conversion/data_out/dsa-2023-0003"# change to your output directory (where you want to save the images extracted)
+
+pdf_name = "dsa-2023-0003"# change to your pdf name
+
+pages <- magick::image_read_pdf(paste0(input_path,"/",pdf_name,".pdf"))
 
 # view the ppng file for each page 
 
@@ -19,7 +24,7 @@ pages
 
 j <- 1:20
 for (i in j){
-  pages[i] %>% image_write(., path = paste0("C:/Workspace/R_Scripts/PDF_Image_Conversion/data_out/dsa-2023-0003/image",i,".png"), format = "png")
+  pages[i] %>% image_write(., path = paste0(output_path,"/image_page_",i,"_",pdf_name,".png"), format = "png")
   
 }  
 
